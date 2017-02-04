446584b3847c550218f0ec1a7be81037344b6b47
laozhenqiang <laozhenqiang@laozhenqiangdeMacBook-Air.local>
2017-02-04 15:47:08 +0800

    c

---
+++ CIDemo/CIDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CIDemo
+//
+//  Created by laozhenqiang on 2017/2/4.
+//  Copyright © 2017年 laozhenqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CIDemo/CIDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CIDemo
+//
+//  Created by laozhenqiang on 2017/2/4.
+//  Copyright © 2017年 laozhenqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
